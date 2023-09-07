6f9fa39330ee3a7cf29497f8d36f9e4796fb3ad9
yilian1234 <42062527+yilian1234@users.noreply.github.com>
2023-09-07 20:42:11 +0800

    Create HextoString.py
    0x类型16进制转换为字符串

---
+++ HextoString.py
@@ -0,0 +1,6 @@
+src = 0x534C43444E
+hex_str = hex(src)  # 转换成十六进制字符串
+hex_str = hex_str[2:] if hex_str.startswith('0x') else hex_str  # 去掉前缀 0x
+bytes_data = bytes.fromhex(hex_str)  # 转换成字节串
+str_data = ''.join(chr(byte) for byte in bytes_data)  # 将字节串转换成字符串
+print(str_data)  # 输出 hello world
