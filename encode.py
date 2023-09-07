61da1ad733f7f34412f3a7c6006bfa98d8a91f78
yilian1234 <42062527+yilian1234@users.noreply.github.com>
2023-09-07 22:18:38 +0800

    Update encode.py

--- encode.py
+++ encode.py
@@ -1,5 +1,5 @@
 import binascii
-################将字符串转为16进制
+################将字符串转为16进制，"hello" -> 68656c6c6f
 str_val = 'A quick brown fox'.encode('utf-8')
 hex_val = binascii.hexlify(str_val).decode('utf-8')
 ###############将16进制字符串转为整形列表， '53216A' -> [0x53, 0x21, 0x6A]
